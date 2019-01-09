de205ec1ad58d8599d3b1529f5bbdebc2b167653
kemsakurai <sakurai.kem@gmail.com>
2019-01-10 00:36:51 +0900

    Refacter doPost

--- src/doPost.ts
+++ src/doPost.ts
@@ -3,29 +3,33 @@ import Utils from './Utils';
 export const doPost = (e): GoogleAppsScript.Content.TextOutput => {
   Logger.log('doPost start');
   Logger.log('e>>>', e);
-  var jsonString = e.parameter.feedback;
-  var data = JSON.parse(jsonString);
+  const jsonString = e.parameter.feedback;
+  const data = JSON.parse(jsonString);
 
-  var url = data.url;
-  var browser = JSON.stringify(data.browser);
-  var comment = data.note;
+  const url = data.url;
+  const browser = JSON.stringify(data.browser);
+  const comment = data.note;
 
-  // シート取得
-  var ss = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = ss.getSheetByName(Utils.getFeedBackSheetName());
-  // データ入力
-  var lastRow = sheet.getLastRow();
+  // Get sheet Object
+  const ss = SpreadsheetApp.getActiveSpreadsheet();
+  const sheet = ss.getSheetByName(Utils.getFeedBackSheetName());
+
+  // Save spreadsheet
+  const lastRow = sheet.getLastRow();
   sheet.appendRow([url, browser, comment]);
 
-  var decoded = Utilities.base64Decode(data.img.split(';base64,')[1]);
-  var fileName = Utilities.formatDate(new Date(), 'JST', 'yyyy/MM/dd (E) HH:mm:ss Z') + '.png';
-  var contentType = 'image/png';
+  // Save image png
+  const decoded = Utilities.base64Decode(data.img.split(';base64,')[1]);
+  const fileName = Utilities.formatDate(new Date(), 'JST', 'yyyy/MM/dd (E) HH:mm:ss Z') + '.png';
+  const contentType = 'image/png';
   const blob: GoogleAppsScript.Base.Blob = Utilities.newBlob(decoded, contentType, fileName);
   const folder: GoogleAppsScript.Drive.Folder = DriveApp.getFolderById(id);
-  var file = folder.createFile(blob);
-  var range = sheet.getRange(lastRow + 1, 4);
+  const file = folder.createFile(blob);
+  const range = sheet.getRange(lastRow + 1, 4);
   range.setValue([file.getUrl()]);
-  var output = ContentService.createTextOutput();
+  
+  // return Response
+  const output = ContentService.createTextOutput();
   output.setMimeType(ContentService.MimeType.JSON);
   output.setContent(JSON.stringify({ message: 'success!' }));
   Logger.log(output);
